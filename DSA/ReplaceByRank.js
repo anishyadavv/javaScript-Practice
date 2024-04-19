76516b9a297d94e3c67aeab646423dd99098aed2
anish <anishyadav7853@gmail.com>
2024-04-19 15:12:28 +0530

    replace by rank

---
+++ DSA/ReplaceByRank.js
@@ -0,0 +1,49 @@
+//Replace elements by its rank in the array
+// Problem Statement: Given an array of N integers, the task is to replace each element of the array by its rank in the array.
+
+// Examples:
+
+// Example 1:
+// Input: 20 15 26 2 98 6
+// Output: 4 3 5 1 6 2
+// Explanation: When sorted,the array is 2,6,15,20,26,98. So the rank of 2 is 1,rank of 6 is 2,rank of 15 is 3 and so…
+
+// Example 2:
+// Input: 1 5 8 15 8 25 9
+// Output: 1 2 3 5 3 6 4
+// Explanation: When sorted,the array is 1,5,8,8,9,15,25. So the rank of 1 is 1,rank of 5 is 2,rank of 8 is 3 and so…
+
+// function replace(array) {
+//   const sorted = array.toSorted((a, b) => a - b);
+//   const ans = [];
+//   let distictElement = new Set([...sorted]);
+
+//   array.forEach((item) => {
+//     const rank = Array.from(distictElement).indexOf(item) + 1;
+//     ans.push(rank);
+//   });
+
+//   return ans;
+// }
+
+function replace(array) {
+  let map = new Map();
+  let ans = [];
+  const sorted = array.toSorted((a,b)=>a-b);
+  const distinct = new Set([...sorted]);
+  Array.from(distinct).forEach((item, index) => {
+    map.set(item, index+1);
+  });
+  console.log(map);
+
+  array.forEach((item) => {
+    ans.push(map.get(item));
+  });
+
+  return ans;
+}
+
+const arr = [20, 15, 26, 2, 98, 6];
+const arr1 = [1, 5, 8, 15, 8, 25, 9];
+
+console.log(replace(arr1));
